240cf9209d58c797cd38c66c7096e6a1a92462af
lovepreetsi <32802848+lovepreetsi@users.noreply.github.com>
2018-03-09 11:56:45 +0000

    to link homepoage to game

---
+++ racing_t/Assets/scripts/Home.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Home : MonoBehaviour {
+
+	public void Playgame()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
