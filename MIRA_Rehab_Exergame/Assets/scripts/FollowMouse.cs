89d00df362ecfce2e8dca4b2d6a660460eb180d7
Samuil Stoychev <samuilstojchev@gmail.com>
2018-03-21 14:41:21 +0000

    car - restricted

--- MIRA_Rehab_Exergame/Assets/scripts/FollowMouse.cs
+++ MIRA_Rehab_Exergame/Assets/scripts/FollowMouse.cs
@@ -20,12 +20,26 @@ public class FollowMouse : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         pause = sv.getPause();
+
         if (collision == false && pause == false)
         {
             
             mousePosition = Input.mousePosition;
             mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+            mousePosition.x = Mathf.Clamp(mousePosition.x, -6.5f, 6.25f);
+            mousePosition.y = Mathf.Clamp(mousePosition.y, - (Screen.height/200) + 1.2f, (Screen.height/200) - 1.2f);
+
+            //Debug.Log(Screen.height);
+           // mousePosition.y = Mathf.Clamp(mousePosition.x, -6.5f, 6.7f);
+            //if (mousePosition.x < -6.5f || mousePosition.x > 6.7f)
+            //{
+            //    transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+            //}
+            //else
+            //{
             transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+            //}
         }
     }
 
